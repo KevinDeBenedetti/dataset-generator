c4ecf6c810dc59250377051550a2e537b10e4cc5
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-07 20:06:55 +0200

    fix: correct the import of the GenerateDataset component in HomeView

--- apps/client/src/views/HomeView.vue
+++ apps/client/src/views/HomeView.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-import GenerateDataset from '@/components/GenerateDataset.vue'
-=======
 import GenerateDataset from '@/components/dataset/Generate.vue'
->>>>>>> 704b60f (feat: remove DatasetResults and GenerateDataset components, and reorganize code for better structure)
 </script>
 
 <template>
